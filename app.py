bd547b1b70681729d6514d56497ce232137484ba
snehashreecnrc <110520746+snehashreecnrc@users.noreply.github.com>
2026-04-28 20:28:44 +0530

    Fix the changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StanderdScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassfier
 
 #load data
